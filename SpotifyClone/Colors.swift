cdef3d191a76d6b79f9fb251ba4ffe8bd696eaa4
Hazhir Javaheri <hajir.javaheri@gmail.com>
2021-08-29 13:40:46 +0430

    adds Colors

---
+++ SpotifyClone/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  SpotifyClone
+//
+//  Created by Hajir Javaheri on 6/7/1400 AP.
+//
+
+import SwiftUI
+
+let darkBlue = Color.init(red: 60/255, green: 90/255, blue: 115/255)
+let magenta = Color.init(red: 160/255, green: 0/255, blue: 115/255)
+let green = Color.init(red: 30/255, green: 215/255, blue: 96/255)
