3c2e4cba7067c75dba1570095ccf02a8795deca6
Hazhir Javaheri <hajir.javaheri@gmail.com>
2021-08-29 13:22:31 +0430

    Initial Commit

---
+++ SpotifyClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpotifyClone
+//
+//  Created by Hajir Javaheri on 6/7/1400 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Hajir Javaheri on 6/7/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
