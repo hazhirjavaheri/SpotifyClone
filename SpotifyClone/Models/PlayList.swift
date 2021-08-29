105583178dede88dfcba83550ac400104c32c7a2
Hazhir Javaheri <hajir.javaheri@gmail.com>
2021-08-29 13:43:30 +0430

    adds data model for playlist

---
+++ SpotifyClone/Models/PlayList.swift
@@ -0,0 +1,24 @@
+//
+//  PlayList.swift
+//  SpotifyClone
+//
+//  Created by Hajir Javaheri on 6/7/1400 AP.
+//
+
+import Foundation
+
+var playList = [
+    Song(name: "Symphony No.5", artist: "Ludvig von Beethoven", artwork: "Beethoven", isFavorite: false),
+    Song(name: "Violin Romance No.2", artist: "Ludvig von Beethoven", artwork: "Beethoven", isFavorite: true),
+    Song(name: "Egmont Overture", artist: "Ludvig von Beethoven", artwork: "Beethoven", isFavorite: false),
+    Song(name: "Ave Maria", artist: "Franz Schubert", artwork: "Schubert", isFavorite: true),
+    Song(name: "Impromptu in E flat major", artist: "Franz Schubert", artwork: "Schubert", isFavorite: false),
+    Song(name: "Ständchen", artist: "Franz Schubert", artwork: "Schubert", isFavorite: true),
+    Song(name: "The 4 Seasons Concerto No.1 in E major Spring", artist: "Antovio Vivaldi",
+         artwork: "Vivaldi", isFavorite: false),
+    Song(name: "Siciliano", artist: "Antovio Vivaldi", artwork: "Vivaldi", isFavorite: true),
+    Song(name: "Concerto for 2 Corni da caccia in F major RV.539 Allegro", artist: "Antovio Vivaldi", artwork: "Vivaldi", isFavorite: false),
+    Song(name: "Eine kleine Nachtmusik Allegro", artist: "Wolfgang A. Mozart", artwork: "Mozart", isFavorite: true),
+    Song(name: "Piano Concerto in A major K 488 Adagio", artist: "Wolfgang A. Mozart", artwork: "Mozart", isFavorite: false),
+    Song(name: "Flute Concerto in D major K.314 Allegro", artist: "Wolfgang A. Mozart", artwork: "Mozart", isFavorite: true),
+    ]
